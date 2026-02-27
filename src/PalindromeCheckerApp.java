db52980a623737a707c70181071dafe9e5c0d25a
Shashank-Prasad7 <shashank.prasad.cr7@gmail.com>
2026-02-27 14:33:18 +0530

    UC1: Add welcome message module

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,9 @@
+
+
+class PalindromeCheckerApp{
+    public static void main(String[] args){
+
+        System.out.println("WELCOME TO THE PALINDROME CHECKER APP :)\nDESCRIPTION:This app checks whther the given input is a palindrome or not!\nAUTHOR : SHASHANK PRASAD");
+
+    }
+}
